7adc9f11080d53cb7ddf4baf22d788196b45ad1e
Ryan Jin <atinjin@gmail.com>
2017-01-30 13:16:03 +0900

    First project created

---
+++ src/main/java/github/atinjin/dimpengine/Dimpengine.java
@@ -0,0 +1,14 @@
+package github.atinjin.dimpengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ryanjin on 30/01/2017.
+ */
+@SpringBootApplication
+public class Dimpengine {
+    public void main(String[] args) {
+        SpringApplication.run(Dimpengine.class, args);
+    }
+}
